51f035cddc3d3ebca5753dc0ff4c6dbe4c77088f
Savro <kevinjevin201@gmail.com>
2025-04-09 02:21:34 +1000

    added mod boilerplate stuff

---
+++ src/main/java/net/savro/abyss/AbyssDataGenerator.java
@@ -0,0 +1,11 @@
+package net.savro.abyss;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AbyssDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
