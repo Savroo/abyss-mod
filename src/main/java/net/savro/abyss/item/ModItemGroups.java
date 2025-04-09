9e0e59494fa9c229025e3976191af2b75c2aa700
Savro <126524984+SavroDev@users.noreply.github.com>
2025-04-09 12:32:13 +1000

    changed some cases and added a group

--- src/main/java/net/savro/abyss/item/ModItemGroups.java
+++ src/main/java/net/savro/abyss/item/ModItemGroups.java
@@ -5,6 +5,7 @@ import net.savro.abyss.Abyss;
 import net.savro.abyss.block.ModBlocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -13,14 +14,23 @@ import net.minecraft.util.Identifier;
 public class ModItemGroups {
     public static final ItemGroup TEST_GROUP = Registry.register(Registries.ITEM_GROUP,
     Identifier.of(Abyss.MOD_ID, "test_group"),
-    FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.Gorple))
+    FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.GORPLE))
         .displayName(Text.translatable("itemGroup.abyss.test_group"))
         .entries((displayContext, entries) -> {
-            entries.add(ModItems.Gorple);
-            entries.add(ModItems.Omaneko);
+            entries.add(ModItems.GORPLE);
+            entries.add(ModItems.OMANEKO);
             entries.add(ModBlocks.GORPLE_BLOCK);
             entries.add(ModBlocks.OMANEKO_BLOCK);
         }).build());
+    
+    public static final ItemGroup CHICKEN_GROUP = Registry.register(Registries.ITEM_GROUP,
+    Identifier.of(Abyss.MOD_ID, "chicken_group"),
+    FabricItemGroup.builder().icon(() -> new ItemStack(Items.COOKED_CHICKEN))
+        .displayName(Text.translatable("itemGroup.abyss.chicken_group"))
+        .entries((displayContext, entries) -> {
+            entries.add(ModBlocks.CHIMKIN_NUGGER);
+        }).build());
+        
     
 
     public static void registerItemGroups() {
