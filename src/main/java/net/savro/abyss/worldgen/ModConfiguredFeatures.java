c6f9f0d3e3fb80ef71fd5cf097679f06dec43a11
Savro <126524984+SavroDev@users.noreply.github.com>
2025-04-09 16:47:08 +1000

    I fixed it :3 >w<

--- src/main/java/net/savro/abyss/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/savro/abyss/worldgen/ModConfiguredFeatures.java
@@ -1,26 +1,26 @@
 package net.savro.abyss.worldgen;
 
 import net.savro.abyss.Abyss;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstrapContext;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
 
 public class ModConfiguredFeatures {
-
-    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
-
+    public static void bootstrap(Registry<ConfiguredFeature<?, ?>> context) {
+        
     }
 
-    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
-        return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(Abyss.MOD_ID, name));
+    public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, Identifier.of(Abyss.MOD_ID, name));
     }
 
-    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
-        ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
-        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    private static <FC extends FeatureConfig, F extends Feature<FC>> void register(Registry<ConfiguredFeature<?, ?>> context,
+            RegistryKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        Registry.register(context, key, new ConfiguredFeature<>(feature, configuration));
     }
 }
