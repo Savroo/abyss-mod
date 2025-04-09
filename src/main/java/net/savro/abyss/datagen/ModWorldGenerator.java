2d67aea16b0e545388c22ff1a752b03c4e870349
Savro <126524984+SavroDev@users.noreply.github.com>
2025-04-09 23:14:41 +1000

    Added a super deep water dimension for testing
    The databuilder is still broken
    use "execute in abyss:abyssdim tp @s ~ 1000 ~" to visit the new dimension

---
+++ src/main/java/net/savro/abyss/datagen/ModWorldGenerator.java
@@ -0,0 +1,25 @@
+package net.savro.abyss.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenerator extends FabricDynamicRegistryProvider {
+    public ModWorldGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+        // Register your world generation features here
+        entries.addAll(registries.getWrapperOrThrow(RegistryKeys.DIMENSION_TYPE));
+    }
+
+    @Override
+    public String getName() {
+        return "World Gen";
+    }
+}
